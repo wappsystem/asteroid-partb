8f1a8ce79c60f23d65eeb3e3f9fa07272e0a339b
Zhiming Zhou <zzm.vmiis@gmail.com>
2022-02-10 19:30:20 +1100

    update

---
+++ modules/module-list.js
@@ -0,0 +1,16 @@
+(function(){
+    //-------------------------------------------------------------------------------------
+    var p=""; //put a unique prefix to avoid conflict with others 
+    var H=$vm.hosting_path+"/modules";
+    var m=$vm.module_list;
+    var api="wapp";
+    //-------------------------------------------------------------------------------------
+    //m[p+"screening"]     ={url:H+"/business/screening/form.html", };
+    /*
+    m[p+"roster-data"]      ={url:H+"/business/roster/data.html",       Table:"staff-roster",  form_module:"roster-form"},
+    m[p+"roster-form"]      ={url:H+"/business/roster/form.html",       Table:"staff-roster"}
+    m[p+"month"]            ={url:H+"/business/calendar/month.html",    Table:"staff-roster","booking":p+"roster-form",router:1};
+    m[p+"week"]             ={url:H+"/business/calendar/week.html",     Table:"staff-roster","booking":p+"roster-form","booking2":p+"roster-form-2",router:1};
+    m[p+"day"]              ={url:H+"/business/calendar/day.html",      Table:"staff-roster","booking":p+"roster-form",router:1};
+    */
+})();
